7bf214e8eb3534a942acdef36233f2d8f3ba355b
xuda <xuda.it@outlook.com>
2018-11-03 22:06:47 +0800

    支持图标

--- src/main/java/com/pro/warehouse/util/DateUtil.java
+++ src/main/java/com/pro/warehouse/util/DateUtil.java
@@ -3,6 +3,7 @@ package com.pro.warehouse.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateUtil {
     public static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -23,5 +24,23 @@ public class DateUtil {
         dateStr = format.format(date);
         return dateStr;
 
+    }
+
+    /**
+     * 获得制定日期前多少天的日期
+     * @param today
+     * @param beforeDays
+     * @return
+     */
+    public static Date getDateBefore(Date today, int beforeDays){
+        //获取当前时间毫秒数
+        long time= today.getTime();
+        //获取7天的毫秒数
+        long sevenTime = beforeDays*24*60*60*1000;
+        //当前时间毫秒数-7天的毫秒数=7天之间那天的毫秒数
+        long times = time - sevenTime;
+        //将毫秒数转日期
+        Date dat=new Date(times);
+        return dat;
     }
 }
